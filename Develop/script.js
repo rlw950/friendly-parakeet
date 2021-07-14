6421ad805cf85634a8052049eea30e0fd738112b
Ray Computer <rlw950@yahoo.com>
2021-07-14 10:46:51 -0700

    password-generator

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,52 @@
 // Assignment code here
-
-
-// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+var uncap = "abcdefghijklmnopqrstuvwxyz";
+var cap = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var number = "0123456789";
+var symbol = ' !"#$%&' + "'()*+,-./:;<=>?@[\]^_`{|}~)";
+var formula = "";
+var password = "";
+
+
+// generatePassword function
+var generatePassword = function() {
+  
+  var quantity = prompt('How may characters do you want (between 8 and 128)?');
+
+  var yesUncap = confirm("Do you want uncapitalized letters?");
+  if (yesUncap) {
+    formula += uncap;
+  }
+
+  var yesCap = confirm("Do you want capitalized letters?");
+  if (yesCap) {
+    formula += cap;
+  }
+
+  var yesNum = confirm("Do you want numbers?");
+  if (yesNum) {
+    formula += number;
+  }
+
+  var yesSymbol = confirm("Do you want symbols?");
+  if (yesSymbol) {
+    formula += symbol;
+  }
+  // console.log(formula, formula.length);
+  
+    for (let i = 0; i < quantity; i++) {
+      var element = formula[Math.floor(Math.random()*formula.length)]
+      password += element;
+    }
+};
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
+var writePassword = function() {
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+generatePassword();
